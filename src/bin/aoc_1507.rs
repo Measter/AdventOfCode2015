e34e8ba449c125793afaf113e9af520b7460280b
Stuart Haidon <serayen.sh@gmail.com>
2020-11-24 19:10:25 +0000

    Implemented days 7 and 8.

---
+++ src/bin/aoc_1507.rs
@@ -0,0 +1,318 @@
+use color_eyre::eyre::{eyre, Result};
+
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq)]
+enum Input {
+    Number(u16),
+    Wire(String),
+}
+
+#[derive(Debug, PartialEq)]
+enum Component {
+    Const {
+        input: Input,
+        output: String,
+    },
+    And {
+        input_a: Input,
+        input_b: Input,
+        output: String,
+    },
+    Or {
+        input_a: Input,
+        input_b: Input,
+        output: String,
+    },
+    LShift {
+        input_a: Input,
+        input_b: Input,
+        output: String,
+    },
+    RShift {
+        input_a: Input,
+        input_b: Input,
+        output: String,
+    },
+    Not {
+        input: Input,
+        output: String,
+    },
+}
+
+#[derive(Debug, PartialEq)]
+struct Circuit {
+    wires: HashMap<String, Option<u16>>,
+    components: Vec<Component>,
+}
+
+impl Circuit {
+    fn get_wire_val(&self, key: &Input) -> Option<u16> {
+        match key {
+            Input::Number(n) => Some(*n),
+            Input::Wire(key) => self.wires.get(key).copied().flatten(),
+        }
+    }
+
+    fn evaluate(&mut self, mut exit: impl FnMut(&Self) -> bool) {
+        while exit(self) {
+            for comp in &self.components {
+                use Component::*;
+                match comp {
+                    Const { input, output } => {
+                        if let (Some(val), Some(output @ None)) =
+                            (self.get_wire_val(input), self.wires.get_mut(output))
+                        {
+                            *output = Some(val);
+                        }
+                    }
+
+                    Not { input, output } => {
+                        if let (Some(val), Some(output @ None)) =
+                            (self.get_wire_val(input), self.wires.get_mut(output))
+                        {
+                            *output = Some(!val);
+                        }
+                    }
+
+                    And {
+                        input_a,
+                        input_b,
+                        output,
+                    } => {
+                        if let (Some(a), Some(b), Some(output @ None)) = (
+                            self.get_wire_val(input_a),
+                            self.get_wire_val(input_b),
+                            self.wires.get_mut(output),
+                        ) {
+                            *output = Some(a & b);
+                        }
+                    }
+
+                    Or {
+                        input_a,
+                        input_b,
+                        output,
+                    } => {
+                        if let (Some(a), Some(b), Some(output @ None)) = (
+                            self.get_wire_val(input_a),
+                            self.get_wire_val(input_b),
+                            self.wires.get_mut(output),
+                        ) {
+                            *output = Some(a | b);
+                        }
+                    }
+
+                    LShift {
+                        input_a,
+                        input_b,
+                        output,
+                    } => {
+                        if let (Some(a), Some(b), Some(output @ None)) = (
+                            self.get_wire_val(input_a),
+                            self.get_wire_val(input_b),
+                            self.wires.get_mut(output),
+                        ) {
+                            *output = Some(a << b);
+                        }
+                    }
+
+                    RShift {
+                        input_a,
+                        input_b,
+                        output,
+                    } => {
+                        if let (Some(a), Some(b), Some(output @ None)) = (
+                            self.get_wire_val(input_a),
+                            self.get_wire_val(input_b),
+                            self.wires.get_mut(output),
+                        ) {
+                            *output = Some(a >> b);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn parse_circuit(input: &str) -> nom::IResult<&str, Circuit> {
+        use nom::{
+            branch::alt,
+            bytes::complete::{tag, take_till1, take_while1},
+            sequence::{separated_pair, tuple},
+        };
+
+        let mut wires = HashMap::new();
+        let mut components = Vec::new();
+
+        for line in input.lines().map(str::trim) {
+            let (_, (component, output)) =
+                separated_pair(take_till1(|c| c == '-'), tag("->"), take_while1(|_| true))(line)?;
+
+            let (_, component) = alt((
+                tuple((
+                    take_till1(|c| c == ' '),
+                    tag(" AND "),
+                    take_while1(|_| true),
+                )),
+                tuple((take_till1(|c| c == ' '), tag(" OR "), take_while1(|_| true))),
+                tuple((
+                    take_till1(|c| c == ' '),
+                    tag(" RSHIFT "),
+                    take_while1(|_| true),
+                )),
+                tuple((
+                    take_till1(|c| c == ' '),
+                    tag(" LSHIFT "),
+                    take_while1(|_| true),
+                )),
+                tuple((tag("NOT "), take_till1(char::is_whitespace), tag(""))),
+                tuple((
+                    take_while1(|c: char| c.is_ascii_alphanumeric()),
+                    tag(""),
+                    tag(""),
+                )),
+            ))(component.trim())?;
+
+            let output = output.trim().to_owned();
+            wires.entry(output.clone()).or_default();
+
+            let mut parse_input = |input: &str| {
+                if let Ok(c) = input.parse() {
+                    Input::Number(c)
+                } else {
+                    wires.entry(input.to_owned()).or_default();
+                    Input::Wire(input.to_owned())
+                }
+            };
+
+            let component = match component {
+                (a, " RSHIFT ", b) => Component::RShift {
+                    input_a: parse_input(a),
+                    input_b: parse_input(b),
+                    output,
+                },
+                (a, " OR ", b) => Component::Or {
+                    input_a: parse_input(a),
+                    input_b: parse_input(b),
+                    output,
+                },
+                (a, " AND ", b) => Component::And {
+                    input_a: parse_input(a),
+                    input_b: parse_input(b),
+                    output,
+                },
+                (a, " LSHIFT ", b) => Component::LShift {
+                    input_a: parse_input(a),
+                    input_b: parse_input(b),
+                    output,
+                },
+
+                ("NOT ", a, _) => Component::Not {
+                    input: parse_input(a),
+                    output,
+                },
+                (a, _, _) => Component::Const {
+                    input: parse_input(a),
+                    output,
+                },
+            };
+
+            components.push(component);
+        }
+
+        Ok(("", Circuit { wires, components }))
+    }
+}
+
+fn part_1(input: &str) -> Result<u16> {
+    let (_, mut circuit) =
+        Circuit::parse_circuit(&input).map_err(|e| eyre!("Parse Error: {}", e))?;
+    let mut counter = 0;
+    circuit.evaluate(|c| {
+        counter += 1;
+        c.wires.values().any(Option::is_none) && counter < 1000
+    });
+
+    circuit
+        .wires
+        .get("a")
+        .copied()
+        .flatten()
+        .ok_or_else(|| eyre!("Wire not found: a"))
+}
+
+fn part_2(input: &str) -> Result<u16> {
+    let (_, mut circuit) =
+        Circuit::parse_circuit(&input).map_err(|e| eyre!("Parse Error: {}", e))?;
+
+    *circuit.wires.get_mut("b").unwrap() = Some(46065);
+
+    let mut counter = 0;
+    circuit.evaluate(|c| {
+        counter += 1;
+        c.wires.values().any(Option::is_none) && counter < 1000
+    });
+
+    circuit
+        .wires
+        .get("a")
+        .copied()
+        .flatten()
+        .ok_or_else(|| eyre!("Wire not found: a"))
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let input = std::fs::read_to_string("inputs/aoc_1507.txt")?;
+
+    let part_1 = part_1(&input)?;
+    println!("Part 1 output: {:?}", part_1);
+
+    let part_2 = part_2(&input)?;
+    println!("Part 2 output: {:?}", part_2);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests_1507 {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let circuit_str = "123 -> x
+        456 -> y
+        x AND y -> d
+        x OR y -> e
+        x LSHIFT 2 -> f
+        y RSHIFT 2 -> g
+        NOT x -> h
+        NOT y -> i";
+
+        let expected: HashMap<_, _> = [
+            ("d".to_owned(), Some(72)),
+            ("e".to_owned(), Some(507)),
+            ("f".to_owned(), Some(492)),
+            ("g".to_owned(), Some(114)),
+            ("h".to_owned(), Some(65412)),
+            ("i".to_owned(), Some(65079)),
+            ("x".to_owned(), Some(123)),
+            ("y".to_owned(), Some(456)),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let (_, mut circuit) = Circuit::parse_circuit(circuit_str).unwrap();
+
+        let mut counter = 0;
+        circuit.evaluate(|c| {
+            counter += 1;
+            c.wires.values().any(Option::is_none) && counter < 100
+        });
+
+        assert_eq!(expected, circuit.wires);
+    }
+}
---
+++ src/bin/aoc_1508.rs
@@ -0,0 +1,123 @@
+use color_eyre::eyre::Result;
+
+fn part1(input: &str) -> (usize, usize) {
+    let mut rendered = String::with_capacity(input.len());
+
+    let mut input_rem = input;
+    loop {
+        match input_rem.as_bytes() {
+            [b'"', ..] => {
+                input_rem = &input_rem[1..];
+            }
+            [b'\\', b'\\', ..] => {
+                rendered.push('\\');
+                input_rem = &input_rem[2..];
+            }
+            [b'\\', b'"', ..] => {
+                rendered.push('"');
+                input_rem = &input_rem[2..];
+            }
+            [b'\\', b'x', a, b, ..] if a.is_ascii_hexdigit() && b.is_ascii_hexdigit() => {
+                let a = match a.to_ascii_lowercase() {
+                    b'0'..=b'9' => a - b'0',
+                    b'a'..=b'f' => a - b'a',
+                    _ => *a,
+                };
+
+                let b = match b.to_ascii_lowercase() {
+                    b'0'..=b'9' => b - b'0',
+                    b'a'..=b'f' => b - b'a',
+                    _ => *b,
+                };
+
+                let ch = (a << 4 | b) as char;
+                rendered.push(ch);
+                input_rem = &input_rem[4..];
+            }
+            [_, ..] => {
+                let ch = input_rem.chars().next().unwrap(); // We know it's not empty.
+                rendered.push(ch);
+                input_rem = &input_rem[ch.len_utf8()..];
+            }
+            [] => break,
+        }
+    }
+
+    (input.len(), rendered.chars().count())
+}
+
+fn part2(input: &str) -> (usize, usize) {
+    let mut rendered = String::new();
+    rendered.push('"');
+
+    for c in input.chars() {
+        match c {
+            '"' => rendered.push_str(r#"\""#),
+            '\\' => rendered.push_str(r#"\\"#),
+            _ => rendered.push(c),
+        }
+    }
+
+    rendered.push('"');
+
+    (input.len(), rendered.len())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let input = std::fs::read_to_string("inputs/aoc_1508.txt")?;
+
+    let part_1: usize = input
+        .lines()
+        .map(part1)
+        .map(|(code, rendered)| code - rendered)
+        .sum();
+    println!("Part 1 output: {:?}", part_1);
+
+    let part_2: usize = input
+        .lines()
+        .map(part2)
+        .map(|(code, rendered)| rendered - code)
+        .sum();
+    println!("Part 2 output: {:?}", part_2);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests_1508 {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        let tests = [
+            (r#""""#, 2, 0),
+            (r#""abc""#, 5, 3),
+            (r#""aaa\"aaa""#, 10, 7),
+            (r#""\x27""#, 6, 1),
+        ];
+
+        for &(input, expected_code, expected_string) in &tests {
+            let (code, string) = part1(input);
+            assert_eq!(code, expected_code, "{}", input);
+            assert_eq!(string, expected_string, "{}", input);
+        }
+    }
+
+    #[test]
+    fn part2_example() {
+        let tests = [
+            (r#""""#, 2, 6),
+            (r#""abc""#, 5, 9),
+            (r#""aaa\"aaa""#, 10, 16),
+            (r#""\x27""#, 6, 11),
+        ];
+
+        for &(input, expected_code, expected_string) in &tests {
+            let (code, string) = part2(input);
+            assert_eq!(code, expected_code, "{}", input);
+            assert_eq!(string, expected_string, "{}", input);
+        }
+    }
+}
